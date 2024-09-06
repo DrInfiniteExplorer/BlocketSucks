5d70e5e0d3a8341dc96de867cb802f91611b8ae5
Pierre Le Moine <git@luben.se>
2024-09-06 13:45:26 +0200

    Yee feck blocket multiple categories is possible!

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import re
 from playwright.sync_api import Playwright, sync_playwright, expect, Route
 
 def is_fordon(entry):
-    for cat in entry['category']:
-        if cat['id'] == "1000":
+    for cat in entry.get('category', {}):
+        if cat.get('id', None) == "1000":
             print("FILTERING CRAP")
             return True
 
@@ -12,13 +12,19 @@ def filter_crap(lst):
     return [entry for entry in lst if not is_fordon(entry)]
 
 def handle_route(route: Route) -> None:
-    response = route.fetch()
+
+    #response = route.fetch()
+    url = f"{route.request.url}&cg=4000&cg=2000&cg=5000&cg=6000&cg=8000"
+    print(f">>> {url}")
+    response = route.fetch(url=url)
     body = response.text()
 
     data = json.loads(body)
 
-    data['data'] = filter_crap(data['data'])
-    data['gallery'] = filter_crap(data['gallery'])
+    if "data" in data:
+        data['data'] = filter_crap(data['data'])
+    if "gallery" in data:
+        data['gallery'] = filter_crap(data['gallery'])
 
     body = json.dumps(data, indent=2)
     #print(body)
@@ -38,8 +44,8 @@ def run(playwright: Playwright) -> None:
 
     page.wait_for_load_state()
 
-    page.on("request", lambda request: print(">>", request.method, request.url))
-    page.on("response", lambda response: print("<<", response.status, response.url))
+    #page.on("request", lambda request: print(">>", request.method, request.url))
+    #page.on("response", lambda response: print("<<", response.status, response.url))
     page.route("**/v2/content?**", handle_route)
 
 
